831f19e19a83140236cf8e7caae185ca5242b280
Moustafa Hassanin <moustafa.mhassanin@gmail.com>
2024-06-19 15:28:25 +0300

    update changes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,6 +3,7 @@ import gulp from 'gulp';
 import concat from 'gulp-concat';
 import prefix from 'gulp-autoprefixer';
 import replace from 'gulp-replace';
+import browserSync from 'browser-sync';
 
 
 
