be4fa1a2f64eace3f98ed1cc2bb42da66a28ff98
Moustafa Hassanin <moustafa.mhassanin@gmail.com>
2024-06-19 15:29:06 +0300

    update changes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,6 +4,9 @@ import concat from 'gulp-concat';
 import prefix from 'gulp-autoprefixer';
 import replace from 'gulp-replace';
 import browserSync from 'browser-sync';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
 
 
 
