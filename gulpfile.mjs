16717e66af62b69cb44bae2b5276d5b89525fef4
Moustafa Hassanin <moustafa.mhassanin@gmail.com>
2024-06-19 15:27:39 +0300

    update changes

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,1 +1,11 @@
-import gulp from 'gulp'; 
+// Gulp to node_modules
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import prefix from 'gulp-autoprefixer';
+import replace from 'gulp-replace';
+
+
+
+// Tasks
+export const css = () => gulp
+.src('styles/main.scss')
